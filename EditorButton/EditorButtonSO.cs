a88af0a7ed65c2a9fc2117477dced4a01a3d3bd1
WyattThornbury <wmthornbury@gmail.com>
2020-03-28 16:18:27 -0700

    EditorButton
    EditorButton is an attribute that can be added to a method that will allow you to call it from a button in the inspector!

---
+++ EditorButton/EditorButtonSO.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[System.AttributeUsage(System.AttributeTargets.Method)]
+public class EditorButtonSOAttribute : PropertyAttribute
+{
+
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(ScriptableObject), true)]
+public class EditorButtonSO : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        var so = target as ScriptableObject;
+
+        var methods = so.GetType()
+            .GetMembers(BindingFlags.Instance | BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public |
+                        BindingFlags.NonPublic)
+            .Where(o => Attribute.IsDefined(o, typeof(EditorButtonSOAttribute)));
+
+        foreach (var memberInfo in methods)
+        {
+            if (GUILayout.Button(memberInfo.Name))
+            {
+                var method = memberInfo as MethodInfo;
+                method.Invoke(so, null);
+                EditorUtility.SetDirty(so);
+            }
+        }
+    }
+}
+#endif
